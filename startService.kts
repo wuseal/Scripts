765e027ef6f7625ae84b01727f9c33b6cc86afb6
Seal <sealkingking@163.com>
2022-01-13 11:18:10 +0800

    Create startService.kts

---
+++ startService.kts
@@ -0,0 +1,32 @@
+@file:Include("https://raw.githubusercontent.com/wuseal/Scripts/main/CommandLine.kt")
+
+/**
+ * kscript url
+ */
+
+fun obtainScriptFromUrl(url: String) = evalBash(url).run {
+    if (exitCode == 0) sout() else ""
+}
+
+"java -version".runCommand()
+
+var script: String = obtainScriptFromUrl(args[0])
+
+var currentProcess : Process? = null
+
+while (true) {
+    val newScript: String = obtainScriptFromUrl(args[0])
+
+    if (newScript.isNotEmpty() && newScript != script) {
+        println("Found Job Script Changed in startService ${args[0]}, Start to execute new Job Script....")
+        currentProcess?.let {
+            it.descendants().forEach { it.destroy() }
+            it.destroy()
+        }
+        currentProcess = "kscript ${args[0]}".runCommand(0)
+        script = newScript
+    } else {
+        println("Job Script not Change, Delay 1 minutes to check again....")
+        Thread.sleep(60 * 1000)
+    }
+}
