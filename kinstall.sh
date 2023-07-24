3c03c9921cca81ccf6266f128fb246ec53996d9c
Seal <sealkingking@163.com>
2023-07-24 14:52:44 +0800

    Update kinstall.sh

--- kinstall.sh
+++ kinstall.sh
@@ -8,7 +8,7 @@ _in_path kscript && echo "kscript is already installed at $(which kscript)" 1>&2
 
     sdkman_auto_answer=true
 
-    _in_path java || echo_and_eval sdk install java
+    _in_path java || echo_and_eval sdk install java 11.0.20-ms
     _in_path kotlin || echo_and_eval sdk install kotlin 1.8.20
     _in_path gradle || echo_and_eval sdk install gradle
     _in_path kscript || echo_and_eval sdk install kscript 3.1.0
